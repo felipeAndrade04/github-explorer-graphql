68aaf3a9b3a29587832fefb41575990c44b8af6d
Felipe Andrade <flpandrade2011@gmail.com>
2021-10-14 21:40:23 -0300

    Add Github personal access token to client

--- src/config/client-graphql.ts
+++ src/config/client-graphql.ts
@@ -1,11 +1,22 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 const link = createHttpLink({
   uri: process.env.REACT_APP_GITHUB_API_URL,
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN;
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link,
+  link: authLink.concat(link),
   cache: new InMemoryCache(),
 });
 
