e34cef87d4051482a293ad1900598e09c75e2521
Felipe Andrade <flpandrade2011@gmail.com>
2021-10-18 21:32:02 -0300

    Add react-toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { ApolloProvider } from '@apollo/client';
+import { ToastContainer } from 'react-toastify';
 
 import client from './config/client-graphql';
 
 import Home from './page/Home';
 import GlobalStyle from './styles/global';
 
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
   return (
     <ApolloProvider client={client}>
       <Home />
+      <ToastContainer />
       <GlobalStyle />
     </ApolloProvider>
   );
